dbfe01d8d073640badb08858fbf295b93e1b58c9
DimitriRO-IBM <dimitri.rodrigues-ISC_France@fr.ibm.com>
2022-11-10 19:29:29 +0100

    fix an issue with an effect exporting a default component

--- log_displayer/pages/index.tsx
+++ log_displayer/pages/index.tsx
@@ -1,6 +1,18 @@
+import { Constants } from '../lib/constants';
 import { NextProgressbarSpinner } from 'nextjs-progressbar-spinner/dist/cjs';
-import { useLogs } from '../pages/effects/logs';
+import useSWR from 'swr';
 import { LogTable } from '../components/log-table/log-table';
+import fetcher = Constants.fetcher;
+
+function useLogs() {
+    const { data, error } = useSWR('api/logs', fetcher);
+
+    return {
+        logs: data,
+        isLoading: !data && !error,
+        isError: error,
+    }
+}
 
 export default function HomeApp() {
     const { logs, isLoading, isError } = useLogs();
